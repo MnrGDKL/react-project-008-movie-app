e2bc383a1571a333b95499a3584b90d4ac723b56
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-11 22:21:01 +0300

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  // const currentUser = {displayName: "munir gedikli"}
+  const currentUser = null;
+  const isLoggedIn = currentUser !== null;
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
         <div className="container-fluid">
-          <Link to={"/"} className="nabar-brand text-white">
+          <Link to={"/"} className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
+            <div className="d-flex text-white align-items-center">
+              {isLoggedIn ? (
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser?.displayName}
+                </h5>
+              ) : <button className="ms-2 btn btn-outline-light"
+                          onClick={()=> navigate("/login")}>Login</button>} 
+              {currentUser ? (
+                <button className="ms-2 btn btn-outline-light">Logout</button>) : (
+                <button className="ms-2 btn btn-outline-light"
+                        onClick={()=> navigate("/register")}>Register</button>)}
+            </div>
         </div>
       </nav>
     </div>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,24 @@
 import React from "react";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+  <div>
+    <div className="d-flex justify-content-center">
+      <div className="form-image">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3">Register</h1>
+        <form id="register">
+          <label className="form-label" htmlFor="first-name">First Name</label>
+          <input id="first-name" type="text" className="form-control" placeholder="Enter Your First Name.." />
+          <label className="form-label" htmlFor="first-name">First Name</label>
+          <input id="first-name" type="text" className="form-control" placeholder="Enter Your First Name.." />
+
+        </form>
+      </div>
+    </div>
+  </div>);
 };
 
 export default Register;
