b7c44792719f54975d1e0b4541cd10c3fc3aefc8
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-13 16:23:07 +0300

    commited

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -12,12 +12,12 @@ import { Navigate } from "react-router-dom";
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: "AIzaSyCXhVo3ua5cnpnG4IzIo23czXD-C0F9d8Q",
-  authDomain: "movie-app-mnrgdkl.firebaseapp.com",
-  projectId: "movie-app-mnrgdkl",
-  storageBucket: "movie-app-mnrgdkl.appspot.com",
-  messagingSenderId: "207634310592",
-  appId: "1:207634310592:web:f0b2ceb049ffc3f6ef9425"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
