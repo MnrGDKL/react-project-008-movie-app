b9a7a18a22a77bdabdf6924ae4e40a1ebebe1712
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-14 00:10:03 +0300

    commit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import MovieCard from "../components/MovieCard";
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 const API_KEY = "63bf2c077a4e69193655625e368b2409";
 
@@ -10,8 +11,9 @@ const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}
 
 
 const Main = () => {
+  const currentUser = useContext(AuthContext);
 const [movies, setMovies] = useState([]);
-
+const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
     getMovies(FEATURED_API);
   }, [])
@@ -27,9 +29,35 @@ const [movies, setMovies] = useState([]);
       .catch((err) => {console.log(err);});
   }
 
-  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchUrl = SEARCH_API + searchTerm;
+    if (searchTerm && currentUser) {
+      getMovies(searchUrl);
+    }
+    else if (!currentUser) {
+      alert("Please login to search");
+    }
+    else {
+      alert("Please enter a search term");
+    }
+
+
+  }
+
+
   return (
   <>
+    <form className="search" onSubmit={handleSubmit}>
+        <input
+          type="search"
+          className="search-input"
+          placeholder="Search a movie..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
     <div className="d-flex justify-content-center flex-wrap">
       {movies.map((movie) => (
         <MovieCard key={movie.id} {...movie} />
