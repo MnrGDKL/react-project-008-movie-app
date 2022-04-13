78786f852376fed573d183f1a080f3b8e70a91ba
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-13 23:25:47 +0300

    commit

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -3,21 +3,22 @@ import {
   createUserWithEmailAndPassword, 
   getAuth, 
   GoogleAuthProvider, 
+  onAuthStateChanged, 
   signInWithEmailAndPassword, 
-  signInWithRedirect, 
-  signOut
+  signInWithPopup, 
+  signOut,
+  updateProfile
 } from "firebase/auth";
-import { Navigate } from "react-router-dom";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: "AIzaSyCXhVo3ua5cnpnG4IzIo23czXD-C0F9d8Q",
+  authDomain: "movie-app-mnrgdkl.firebaseapp.com",
+  projectId: "projectId=movie-app-mnrgdkl",
+  storageBucket: "movie-app-mnrgdkl.appspot.com",
+  messagingSenderId: "207634310592",
+  appId: "1:207634310592:web:f0b2ceb049ffc3f6ef9425",
 };
 
 // Initialize Firebase
@@ -26,13 +27,16 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-export const createUser = async (email, password, navigate) => {
+export const createUser = async (email, password, displayName, navigate) => {
   try {
     let userCredential = await createUserWithEmailAndPassword(
       auth, 
       email, 
       password
       );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+      });
     navigate("/");
     console.log(userCredential);
     
@@ -57,17 +61,34 @@ export const signIn = async (email, password, navigate) => {
   }
 }
 
-export const logOut = (navigate) => {
+export const logOut = () => {
   signOut(auth);
   alert("You have been logged out");
-  navigate("/login");
 }
 
 
-export const googleSignIn = async (navigate) => {
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (currentUser) => {
+    if (currentUser) {
+      setCurrentUser(currentUser);
+    } else {
+      setCurrentUser(false);
+    }
+});
+}
+
+
+export const signUpProvider = (navigate) => {
   const provider = new GoogleAuthProvider();
-  provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-  signInWithRedirect(auth, provider);
-  navigate("/");
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      console.log(error);
+      // ...
+    });
 };
 
