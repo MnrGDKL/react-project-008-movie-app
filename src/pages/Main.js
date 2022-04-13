24b0648f4f0528ad532ecc4b612fca89e8b88391
MnrGDKL <munirgedikli.2021@gmail.com>
2022-04-13 23:26:31 +0300

    commit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,41 @@
-import React from "react";
+import axios from "axios";
+import MovieCard from "../components/MovieCard";
+import React, { useEffect, useState } from "react";
 
-const apiKey = "63bf2c077a4e69193655625e368b2409";
-const url = "https://api.themoviedb.org/3/discover/movie?api_key=";
+const API_KEY = "63bf2c077a4e69193655625e368b2409";
+
+
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
 
 const Main = () => {
+const [movies, setMovies] = useState([]);
 
+  useEffect(() => {
+    getMovies(FEATURED_API);
+  }, [])
+  
+
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => {
+        console.log(res.data)
+        setMovies(res.data.results);
+      })
+      .catch((err) => {console.log(err);});
+  }
 
   
-  return <div>Main</div>;
+  return (
+  <>
+    <div className="d-flex justify-content-center flex-wrap">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} {...movie} />
+      ))}
+      </div>
+  </>)
 };
 
 export default Main;
